e0b88fda846e69231d454b02d654c20546747738
Jon McManus <due@wxwhatever.com>
2019-12-02 20:48:34 +1000

    Try again.

--- src/main/java/noobanidus/mods/glimmering/client/data/GLLangProvider.java
+++ src/main/java/noobanidus/mods/glimmering/client/data/GLLangProvider.java
@@ -32,7 +32,7 @@ public class GLLangProvider extends DeferredLanguageProvider {
     add("glimmering.node.type.0", "Glimmer Relay");
     add("glimmering.node.type.1", "Glimmer Transmitter");
     add("glimmering.node.type.2", "Glimmer Receiver");
-    add("glimmering.message.type_change", "This Glimmer is now a %s");
+    add("glimmering.message.type_change", "This Glimmer is now a %s.");
     add("glimmering.tooltip.line0", "Magical energy transfer node. Hit with %s to change mode.");
     add("glimmering.tooltip.line1", "Sneak while attacking to dismantle node.");
 
